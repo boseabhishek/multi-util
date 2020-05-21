ba604f36c68324fdf243913a71aa86a6d982100a
Abhishek Bose <j2ee.abose@gmail.com>
2020-05-21 16:19:45 +0100

    add logging

--- utils/string.go
+++ utils/string.go
@@ -1,8 +1,13 @@
 package utils
 
-import "strings"
+import (
+	"strings"
+	"log"
+)
 
 func Format(unformattedStr string) string {
+	log.Print("string passed::", unformattedStr)
 	t := strings.TrimSpace(unformattedStr)
+	log.Print("string returned::", t)
 	return t
 }
